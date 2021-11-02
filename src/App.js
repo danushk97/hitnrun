9320cc635a03de550eb2a6259d0ea53202667ccb
danushk97 <danushk97@gmail.com>
2021-11-02 20:16:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './football.png';
+import {FaPhone} from "react-icons/fa"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+            <p>Hit N Run Futsal turf, Kalapatti, Coimbatore.</p>
+            <p><FaPhone/> 9751117333, 9751277333</p>
+            <img src={logo} className="App-logo" alt="alt"/>
+            <p>Construction in progress!</p>
       </header>
     </div>
   );
